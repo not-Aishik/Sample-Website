b1a6e3c0f8ba19a1515d16747a4632cfb840de0c
Aishik <ab.19u10136@btech.nitdgp.ac.in>
2020-11-21 01:39:26 +0530

    final commit 04

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+
+
+
+
 /*----------------------WAVE-------------------------------------------------------------------------*/
 
 var i = 0;
